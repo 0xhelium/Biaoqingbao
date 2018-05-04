110b6bc01cb461a59d1fd1385775ce055c733fc8
HAndy2013 <wybpp2013@hotmail.com>
2018-05-04 09:25:24 +0800

    提交代码

---
+++ Biaoqingbao/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Biaoqingbao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnSearch = new System.Windows.Forms.Button();
+            this.TbKeyword = new System.Windows.Forms.TextBox();
+            this.FlowLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.ni = new System.Windows.Forms.NotifyIcon(this.components);
+            this.FlowLayoutHints = new System.Windows.Forms.FlowLayoutPanel();
+            this.CtxMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CtxItemQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.CbCopyGif = new System.Windows.Forms.CheckBox();
+            this.CtxMenuPicbox = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.CtxItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.CtxItemCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.CtxMenu.SuspendLayout();
+            this.CtxMenuPicbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnSearch
+            // 
+            this.BtnSearch.Location = new System.Drawing.Point(301, 7);
+            this.BtnSearch.Name = "BtnSearch";
+            this.BtnSearch.Size = new System.Drawing.Size(75, 23);
+            this.BtnSearch.TabIndex = 2;
+            this.BtnSearch.Text = "搜 索";
+            this.BtnSearch.UseVisualStyleBackColor = true;
+            // 
+            // TbKeyword
+            // 
+            this.TbKeyword.Location = new System.Drawing.Point(7, 8);
+            this.TbKeyword.Name = "TbKeyword";
+            this.TbKeyword.Size = new System.Drawing.Size(290, 21);
+            this.TbKeyword.TabIndex = 1;
+            // 
+            // FlowLayout
+            // 
+            this.FlowLayout.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.FlowLayout.Location = new System.Drawing.Point(0, 80);
+            this.FlowLayout.Name = "FlowLayout";
+            this.FlowLayout.Size = new System.Drawing.Size(468, 208);
+            this.FlowLayout.TabIndex = 2;
+            // 
+            // ni
+            // 
+            this.ni.ContextMenuStrip = this.CtxMenu;
+            this.ni.Icon = ((System.Drawing.Icon)(resources.GetObject("ni.Icon")));
+            this.ni.Text = "notifyIcon1";
+            this.ni.Visible = true;
+            // 
+            // FlowLayoutHints
+            // 
+            this.FlowLayoutHints.Location = new System.Drawing.Point(0, 36);
+            this.FlowLayoutHints.Name = "FlowLayoutHints";
+            this.FlowLayoutHints.Size = new System.Drawing.Size(468, 45);
+            this.FlowLayoutHints.TabIndex = 3;
+            // 
+            // CtxMenu
+            // 
+            this.CtxMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CtxItemQuit});
+            this.CtxMenu.Name = "CtxMenu";
+            this.CtxMenu.Size = new System.Drawing.Size(101, 26);
+            // 
+            // CtxItemQuit
+            // 
+            this.CtxItemQuit.Name = "CtxItemQuit";
+            this.CtxItemQuit.Size = new System.Drawing.Size(100, 22);
+            this.CtxItemQuit.Text = "退出";
+            // 
+            // CbCopyGif
+            // 
+            this.CbCopyGif.AutoSize = true;
+            this.CbCopyGif.Location = new System.Drawing.Point(379, 15);
+            this.CbCopyGif.Name = "CbCopyGif";
+            this.CbCopyGif.Size = new System.Drawing.Size(90, 16);
+            this.CbCopyGif.TabIndex = 5;
+            this.CbCopyGif.Text = "复制Gif格式";
+            this.CbCopyGif.UseVisualStyleBackColor = true;
+            // 
+            // CtxMenuPicbox
+            // 
+            this.CtxMenuPicbox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CtxItemCopy,
+            this.CtxItemSave});
+            this.CtxMenuPicbox.Name = "CtxMenuPicbox";
+            this.CtxMenuPicbox.Size = new System.Drawing.Size(153, 70);
+            // 
+            // CtxItemSave
+            // 
+            this.CtxItemSave.Name = "CtxItemSave";
+            this.CtxItemSave.Size = new System.Drawing.Size(152, 22);
+            this.CtxItemSave.Text = "另存为";
+            // 
+            // CtxItemCopy
+            // 
+            this.CtxItemCopy.Name = "CtxItemCopy";
+            this.CtxItemCopy.Size = new System.Drawing.Size(152, 22);
+            this.CtxItemCopy.Text = "复制";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 288);
+            this.Controls.Add(this.CbCopyGif);
+            this.Controls.Add(this.FlowLayoutHints);
+            this.Controls.Add(this.FlowLayout);
+            this.Controls.Add(this.TbKeyword);
+            this.Controls.Add(this.BtnSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "表情包搜索";
+            this.CtxMenu.ResumeLayout(false);
+            this.CtxMenuPicbox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSearch;
+        private System.Windows.Forms.TextBox TbKeyword;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayout;
+        private System.Windows.Forms.NotifyIcon ni;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayoutHints;
+        private System.Windows.Forms.ContextMenuStrip CtxMenu;
+        private System.Windows.Forms.ToolStripMenuItem CtxItemQuit;
+        private System.Windows.Forms.CheckBox CbCopyGif;
+        private System.Windows.Forms.ContextMenuStrip CtxMenuPicbox;
+        private System.Windows.Forms.ToolStripMenuItem CtxItemSave;
+        private System.Windows.Forms.ToolStripMenuItem CtxItemCopy;
+    }
+}
+
---
+++ Biaoqingbao/Form1.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Biaoqingbao
+{
+    public partial class Form1 : Form
+    {
+        private string BASE_URL = "http://pic.sogou.com/pics/json.jsp?query={0}%20表情&st=5&start=0&xml_len=50&callback=dataCallback&reqFrom=wap_result&";
+        HttpClient Client;
+        Font LabelFont;
+        string TempFolder;
+        bool CopyGif { get { return CbCopyGif.Checked; } }
+        string LastClickPicbox = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            BindEvents();
+
+            ShowInTaskbar = false;
+            Client = new HttpClient();
+            Client.Timeout = new TimeSpan(0, 0, 3);
+            FlowLayout.AutoScroll = true;
+            FlowLayoutHints.AutoScroll = true;
+
+            ni.Visible = true;
+            this.KeyPreview = true;
+
+            LabelFont = new Font(DefaultFont.Name, DefaultFont.Size, FontStyle.Underline);
+            //TempFolder
+            var tempPath = Path.GetTempPath();
+            TempFolder = Path.Combine(tempPath, "biaoqingbao");
+            if (!Directory.Exists(TempFolder))
+            {
+                Directory.CreateDirectory(TempFolder);
+            }
+        }
+
+        private void BindEvents()
+        {
+            this.BtnSearch.Click += async (sender, args) => await SearchAsync();
+            ni.Click += (s, e) =>
+            {
+                var me = e as MouseEventArgs;
+                if (me.Button == MouseButtons.Left)
+                {
+                    Show();
+                    WindowState = FormWindowState.Normal;
+                    this.Activate();
+                }
+            };
+            this.SizeChanged += (s, e) =>
+            {
+                if (WindowState == FormWindowState.Minimized)
+                {
+                    ni.Visible = true;
+                    Hide();
+                }
+            };
+            this.Load += (s, e) =>
+            {
+                var size = Screen.PrimaryScreen.WorkingArea.Size;
+                this.Location = new Point(size.Width - this.Width, size.Height - this.Height);
+                TbKeyword.Focus();
+            };
+            this.KeyDown += async (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter && TbKeyword.ContainsFocus)
+                {
+                    await SearchAsync();
+                }
+            };
+            this.FormClosed += (s, e) =>
+            {
+                if (Directory.Exists(TempFolder))
+                {
+                    System.Diagnostics.Process p = new System.Diagnostics.Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.Start();
+
+                    //向cmd窗口发送输入信息
+                    p.StandardInput.WriteLine(string.Format("rmdir /s/q {0} &exit", TempFolder));
+                }
+            };
+            CtxItemQuit.Click += (s, e) =>
+            {
+                Close();
+            };
+            CtxItemCopy.Click += (s, e) =>
+            {
+                if (imgList.ContainsKey(LastClickPicbox))
+                {
+                    var sender = FlowLayout.Controls.Find(LastClickPicbox, true).FirstOrDefault() as PictureBox;
+                    if (sender!=null)
+                    {
+                        PicBoxDbClick(sender, null);
+                        return;
+                    }
+                }
+                MessageBox.Show("there was something wrong...");
+            };
+            CtxItemSave.Click += (s, e) =>
+            {
+                if (imgList.ContainsKey(LastClickPicbox))
+                {
+                    var img = imgList[LastClickPicbox];
+                    if (!File.Exists(img))
+                    {
+                        MessageBox.Show("there was something wrong...");
+                        return;
+                    }
+                    var fileInfo = new FileInfo(img);
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.Filter = string.Format("Image files(*{0})|*{0}|All files(*.*)|*.*", fileInfo.Extension);
+                    sfd.FileName = DateTime.Now.ToString("yyMMddHHmmssfff");
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        fileInfo.CopyTo(sfd.FileName);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("there was something wrong...");
+                }
+            };
+        }
+
+        private async Task SearchAsync()
+        {
+            var keyword = TbKeyword.Text.Trim();
+            if (keyword.Length > 0)
+            {
+                FlowLayout.Controls.Clear();//图片框
+                FlowLayoutHints.Controls.Clear();//相关词
+                imgList.Clear();
+                BtnSearch.Enabled = false;
+                CancellationTokenSource source = new CancellationTokenSource(30000);
+                var task = Task.Run(() =>
+                {
+                    Search(keyword);
+                }, source.Token);
+                try
+                {
+                    await task;
+                }
+                catch (Exception ex) { }
+                finally
+                {
+                    source.Dispose();
+                    BtnSearch.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("请输入关键词");
+            }
+        }
+
+        private void PicBoxDbClick(object sender, EventArgs e)
+        {
+            var picbox = sender as PictureBox;
+            var sc = new StringCollection();
+            var filename = imgList[picbox.Name];
+            if (CopyGif && !filename.EndsWith(".gif"))
+            {
+                int width = 128;
+                int height = width;
+                int stride = width / 8;
+                byte[] pixels = new byte[height * stride];
+                // Define the image palette
+                BitmapPalette myPalette = BitmapPalettes.WebPalette;
+
+                // Creates a new empty image with the pre-defined palette
+                BitmapSource image = BitmapSource.Create(
+                    width,
+                    height,
+                    96,
+                    96,
+                    PixelFormats.Indexed1,
+                    myPalette,
+                    pixels,
+                    stride);
+                using (FileStream stream = new FileStream(filename, FileMode.OpenOrCreate))
+                {
+                    using (FileStream stream2 = new FileStream(filename + ".gif", FileMode.OpenOrCreate))
+                    {
+                        GifBitmapEncoder encoder = new GifBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(stream));
+                        encoder.Save(stream2);
+                        sc = new StringCollection();
+                        sc.Add(filename + ".gif");
+                        Clipboard.SetFileDropList(sc);
+                    }
+                }
+            }
+            else
+            {
+                sc.Add(imgList[picbox.Name]);
+                Clipboard.SetFileDropList(sc);
+            }
+        }
+
+        private async Task LableClick(object sender, EventArgs e)
+        {
+            var label = sender as Label;
+            TbKeyword.Text = label.Text;
+            await SearchAsync();
+        }
+
+        private Dictionary<string, string> imgList = new Dictionary<string, string>();
+
+        private void Search(string keyword)
+        {
+            var url = string.Format(BASE_URL, keyword);
+            var jsonp = Client.GetStringAsync(url).Result;
+            if (!string.IsNullOrEmpty(jsonp))
+            {
+                var match = Regex.Match(jsonp, @"dataCallback\((.*)\);");
+                if (match.Success)
+                {
+                    jsonp = match.Groups[1].Value;
+                }
+                else
+                {
+                    return;
+                }
+                JavaScriptSerializer jss = new JavaScriptSerializer();
+                var data = jss.Deserialize<TempData>(jsonp);
+                foreach (var word in data.hintWords)
+                {
+                    Label l = new Label();
+                    l.Text = word;
+                    l.AutoSize = true;
+                    l.Font = LabelFont;
+                    l.ForeColor = System.Drawing.Color.Blue;
+                    l.Margin = new Padding(1);
+                    l.Cursor = Cursors.Hand;
+
+                    l.Click += async (s, e) =>
+                    {
+                        await LableClick(s, e);
+                    };
+                    this.Invoke(new Action(() =>
+                    {
+                        FlowLayoutHints.Controls.Add(l);
+                    }));
+                }
+                Parallel.For(0, data.items.Count, new ParallelOptions { MaxDegreeOfParallelism = 5 }, i =>
+                {
+                    try
+                    {
+                        var picUrl = data.items[i].picUrl;
+                        var result = Client.GetAsync(picUrl).Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            using (var fs = result.Content.ReadAsStreamAsync().Result)
+                            {
+                                this.Invoke(new Action(() =>
+                                {
+                                    var pb = new PictureBox();
+                                    pb.Name = Guid.NewGuid().ToString();
+                                    pb.Cursor = Cursors.Hand;
+                                    pb.BackgroundImageLayout = ImageLayout.Stretch;
+                                    pb.ContextMenuStrip = CtxMenuPicbox;
+                                    Bitmap img = Image.FromStream(fs) as Bitmap;
+                                    var fileName = Path.Combine(TempFolder, Guid.NewGuid().ToString() + GetImgExt(img.RawFormat));
+                                    img.Save(fileName);
+                                    imgList.Add(pb.Name, fileName);
+                                    var tImg = img.GetThumbnailImage(100, 100, () => { return false; }, IntPtr.Zero);
+                                    pb.BackgroundImage = tImg;
+                                    if (img.RawFormat.Guid == ImageFormat.Gif.Guid)
+                                    {
+                                        using (var g = Graphics.FromImage(tImg))
+                                        {
+                                            g.DrawImage(Resource.gif, new Point(1, 1));
+                                        }
+                                    }
+                                    img.Dispose();
+
+                                    pb.Size = new Size(100, 100);
+                                    pb.DoubleClick += (s, e) =>
+                                    {
+                                        try
+                                        {
+                                            PicBoxDbClick(s, e);
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            MessageBox.Show("复制失败, 请重试");
+                                        }
+                                    };
+                                    pb.MouseUp += (s, e) =>
+                                    {
+                                        if (e.Button == MouseButtons.Right)
+                                        {
+                                            LastClickPicbox = pb.Name;
+                                        }
+                                    };
+                                    FlowLayout.Controls.Add(pb);
+                                }));
+                            }
+                        }
+                    }
+                    catch { }
+                });
+            }
+        }
+
+        private string GetImgExt(ImageFormat fmt)
+        {
+            if (fmt.Guid == ImageFormat.Bmp.Guid) { return ".bmp"; }
+            if (fmt.Guid == ImageFormat.Emf.Guid) { return ".emp"; }
+            if (fmt.Guid == ImageFormat.Exif.Guid) { return ".exif"; }
+            if (fmt.Guid == ImageFormat.Gif.Guid) { return ".gif"; }
+            if (fmt.Guid == ImageFormat.Icon.Guid) { return ".ico"; }
+            if (fmt.Guid == ImageFormat.Jpeg.Guid) { return ".jpg"; }
+            if (fmt.Guid == ImageFormat.MemoryBmp.Guid) { return ".bmp"; }
+            if (fmt.Guid == ImageFormat.Png.Guid) { return ".png"; }
+            if (fmt.Guid == ImageFormat.Tiff.Guid) { return ".tiff"; }
+            if (fmt.Guid == ImageFormat.Wmf.Guid) { return ".wmf"; }
+            return ".jpg";
+        }
+    }
+
+    class TempData
+    {
+        public IList<TempItem> items { get; set; }
+        public IList<string> hintWords { get; set; }
+    }
+    class TempItem
+    {
+        public string picUrl { get; set; }
+    }
+}
